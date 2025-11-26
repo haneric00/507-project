12bb685768dce3dcd34931bb02484153a41f8d9e
HFeeney <haydenbfeeney@gmail.com>
2025-11-25 16:37:34 -0800

    Remove unnecessary definitions from factory.py

--- factory.py
+++ factory.py
@@ -1,15 +1,3 @@
-""" EXAMPLE USAGE:
-factory = Factory()
-
-furnace1 = factory.new_node('furance1', prod=[('iron_plate', 2)], cons=[('iron_ore', 1)])
-source = factory.new_node('source1', prod=[('iron_ore', 10)], cons=[])
-
-furnace1.add_input(source)
-
-
-print(factory)
-"""
-
 import itertools
 
 class Recipe:
@@ -42,22 +30,6 @@ class FactoryNode:
         if ins_type == 'normal':
             return FactoryNode('inserter_normal', prod=[(from_node.prod[0][0], 0.5)], cons=[(from_node.prod[0][0], 0.5)])
 
-    def max_production(self):
-        return self.prod
-    
-    def max_consumption(self):
-        return self.cons
-    
-    def add_input(self, node):
-        inserter = FactoryNode.inserter('normal', node)
-
-        if node.factory is not None:
-            node.factory.add_node(inserter)
-            inserter.factory = node.factory
-
-        self.sources.add(inserter)
-        inserter.sources.add(node)
-
     def __str__(self):
         return str(self.id) + "_" + self.name
 
@@ -65,12 +37,6 @@ class FactoryNode:
 class Factory:
     def __init__(self):
         self.nodes = set()
-    
-    def new_node(self, *args, **kwargs):
-        node = FactoryNode(*args, **kwargs)
-        self.nodes.add(node)
-        node.factory = self
-        return node
     
     def add_node(self, node):
         self.nodes.add(node)
@@ -80,11 +46,7 @@ class Factory:
         out = ''
         for node in self.nodes:
             out += f"Node: {node.name}\n"
-            out += f"  Max Production: {node.prod}\n"
-            out += f"  Max Consumption: {node.cons}\n"
             out += f"  Sources: {[input_node.name for input_node in node.sources]}\n"
         
         return out
-
-
 
